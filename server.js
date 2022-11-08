0716bebf9622c74b5bc45958f42ec03189f32faf
andr3w <5803467+a-n-d-r-3-w@users.noreply.github.com>
2022-11-08 16:03:17 -0500

    Add helmet and rate limiter

--- server.js
+++ server.js
@@ -2,8 +2,32 @@ const express = require("express");
 const https = require("https");
 const fs = require("fs");
 const compression = require("compression");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
+// Disable app.use(helmet.contentSecurityPolicy()); because it prevents the page from rendering on production builds.
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
+
+// Rate limiter
+// Enable 'trust proxy' if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+// app.set('trust proxy', 1);
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 60, // limit each IP to `max` requests per `windowMs`
+});
+app.use(limiter);
+
 app.use(compression());
 app.use(express.static("public"));
 
