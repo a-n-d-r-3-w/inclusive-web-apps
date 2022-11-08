265626c176fadf528dd8ccc170f6a3b29420baa2
andr3w <5803467+a-n-d-r-3-w@users.noreply.github.com>
2022-11-08 16:11:54 -0500

    Redirect to correct page

--- routers/users.js
+++ routers/users.js
@@ -1,36 +1,36 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const HttpStatus = require('http-status-codes');
-const { createUser } = require('./usersUtils');
+const bodyParser = require("body-parser");
+const express = require("express");
+const { StatusCodes } = require("http-status-codes");
+const { createUser } = require("./usersUtils");
 
 const router = express.Router();
 router.use(bodyParser.urlencoded());
 
-const validateUsername = username => {
+const validateUsername = (username) => {
   if (username.length < 4 || username.length > 20) {
-    throw new Error('That username is too short or too long.');
+    throw new Error("That username is too short or too long.");
   }
 
-  const characters = username.split('');
+  const characters = username.split("");
   const everyCharacterIsValid = characters
-    .map(character => character.charCodeAt())
-    .every(charCode => {
+    .map((character) => character.charCodeAt())
+    .every((charCode) => {
       const isLowercaseLetter = charCode >= 97 && charCode <= 122;
       const isDigit = charCode >= 48 && charCode <= 57;
       return isLowercaseLetter || isDigit;
     });
   if (!everyCharacterIsValid) {
-    throw new Error('That username contains invalid characters.');
+    throw new Error("That username contains invalid characters.");
   }
 };
 
-const validatePassword = password => {
+const validatePassword = (password) => {
   if (password.length < 8 || password.length > 64) {
-    throw new Error('That password is too short or too long.');
+    throw new Error("That password is too short or too long.");
   }
 };
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const { username, password } = req.body;
     validateUsername(username);
@@ -38,15 +38,15 @@ router.post('/', async (req, res) => {
 
     await createUser(username, password);
     res.redirect(
-      HttpStatus.SEE_OTHER,
-      `/create-account/success?username=${username}`
+      StatusCodes.SEE_OTHER,
+      `/create-account.html?success=true&username=${username}`
     );
     return;
   } catch (error) {
     const encodedErrorMessage = encodeURIComponent(error.message); // To replace spaces with %20, for example.
     res.redirect(
-      HttpStatus.SEE_OTHER,
-      `/create-account?error=${encodedErrorMessage}`
+      StatusCodes.SEE_OTHER,
+      `/create-account.html?error=${encodedErrorMessage}`
     );
   }
 });
