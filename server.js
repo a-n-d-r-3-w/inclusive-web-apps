fdb53d94b308d2bf446985afa3395e0469105584
andr3w <5803467+a-n-d-r-3-w@users.noreply.github.com>
2022-11-08 16:00:22 -0500

    Create Account page with API

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(compression());
 app.use(express.static("public"));
 
+app.post("/api/users", (req, res) => {
+  res.send("hi");
+});
+
 const options = {
   key: fs.readFileSync("localhost-key.pem"),
   cert: fs.readFileSync("localhost.pem"),
