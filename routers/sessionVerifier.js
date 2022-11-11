40fba4433ed70830d75b5c030a1cf17820ad2ca2
andr3w <5803467+a-n-d-r-3-w@users.noreply.github.com>
2022-11-11 18:37:38 -0500

    Get session ID cookie on server

--- routers/sessionVerifier.js
+++ routers/sessionVerifier.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const router = express.Router();
+router.use(cookieParser());
 
 router.use((req, res, next) => {
+  const sessionId = req.cookies.sessionId;
+  // Verify if session ID exists in table.
   next();
 });
 
